948dde32649843f3a5eac3711ab6dde903ae70f8
Alexander Fu <aafu@ucdavis.edu>
2021-03-07 11:33:14 -0500

    Fix all conflicts

---
+++ pipeline/pak-tools/bin/copy-provided-data.ts
@@ -0,0 +1,115 @@
+#!/usr/bin/env node
+import 'reflect-metadata';
+import * as fs from 'fs';
+import * as path from 'path';
+import { paths } from '@local/paths';
+import * as unzipper from 'unzipper';
+import {exiftool} from 'exiftool-vendored';
+import fetch from 'node-fetch';
+import { parse } from 'node-html-parser';
+
+const NEWS_OTHER = 'http://ingamenews.satisfactorygame.com/other.news';
+const NEWS_EXPERIMENTAL = 'http://ingamenews.satisfactorygame.com/experimental.news';
+const NEWS_MAIN = 'http://ingamenews.satisfactorygame.com/main.news';
+
+const { execSync } = require("child_process");
+
+import * as prettier from 'prettier';
+
+const DEFAULT_INSTALL_DIR_EXPERIMENTAL = '/mnt/a/Games/Epic/SatisfactoryExperimental';
+const DEFAULT_INSTALL_DIR_RELEASE = '/mnt/a/Games/Epic/SatisfactoryEarlyAccess';
+
+const DEFAULT_PAK_PATH_PART = 'FactoryGame/Content/Paks/FactoryGame-WindowsNoEditor.pak';
+const DEFAULT_EXE_PATH_PART = 'FactoryGame.exe';
+
+
+let INSTALL_DIR = process.env.INSTALL_DIR || DEFAULT_INSTALL_DIR_RELEASE;
+let BRANCH = "master"
+let NEWS_SOURCE = NEWS_MAIN;
+
+main();
+
+async function main() {
+  if (process.env.EXPERIMENTAL) {
+    INSTALL_DIR = process.env.EXPERIMENTAL_INSTALL_DIR || DEFAULT_INSTALL_DIR_EXPERIMENTAL;
+    BRANCH = "experimental"
+    NEWS_SOURCE = NEWS_EXPERIMENTAL;
+
+    console.log(execSync(`cd ${paths.sourceData.root}; git reset --hard; \
+      git checkout experimental; git pull`).toString())
+  } else {
+    console.log(execSync(`cd ${paths.sourceData.root}; git reset --hard; \
+      git checkout master; git pull`).toString())
+  }
+
+  await copyDocs();
+  await copyHeaders();
+  await generateMetadataJson();
+  // await commitFiles();
+}
+
+async function copyDocs(sourceDocFolder = path.join(INSTALL_DIR, 'CommunityResources', 'Docs'),
+                        targetDocFolder = path.join(paths.sourceData.docs)) {
+  console.log(sourceDocFolder);
+  fs.copyFileSync(path.join(sourceDocFolder, 'Docs.json'), path.join(targetDocFolder, 'Docs.json'));
+  console.log("Copied docs.")
+}
+
+async function copyHeaders(sourceHeaderFolder = path.join(INSTALL_DIR, 'CommunityResources'),
+                        targetHeaderFolder = path.join(paths.sourceData.headers)) {
+  await new Promise((resolve) => fs.createReadStream(path.join(sourceHeaderFolder, "Headers.zip"))
+    .pipe(unzipper.Extract({ path: targetHeaderFolder })).on('close', function () {
+      resolve();
+    }));
+  console.log("Copied Headers.")
+}
+
+async function fetchVersions(url: string) {
+  const response = await fetch(url);
+  const body = await response.text();
+
+  const root = parse(body).querySelector('body');
+  if (root.firstChild.rawText === '\n') {
+    const titleString = root.childNodes[1].rawText.split('</>')[0].replace(/\u2013|\u2014/g, "-");
+
+    const versions = titleString.split('-').map(item => item.trim());
+
+    let semanticVersion = ''
+    if (versions[1].charAt(0) == 'v') {
+      semanticVersion = versions[1].substring(1);
+    }
+
+    let buildNumber = 0;
+    if (versions[2].includes('Build ')) {
+      buildNumber = parseInt(versions[2].split('Build ')[1]);
+    }
+
+    if (semanticVersion !== '' && buildNumber != 0) {
+      return {
+        semanticVersion,
+        buildNumber
+      };
+    }
+  }
+
+  throw new Error("Could not extract semantic and build numbers from " + url);
+}
+
+async function generateMetadataJson() {
+  const exeFilePath = path.join(INSTALL_DIR, DEFAULT_EXE_PATH_PART);
+  const tags = await exiftool.read(exeFilePath);
+  await exiftool.end();
+
+  const buildNumber = parseInt((tags as any).ProductVersion.match(/^\+\+FactoryGame\+main-CL-([0-9]+)$/)![1]);
+
+  const news = await fetchVersions(NEWS_SOURCE);
+
+  if (news.buildNumber !== buildNumber) {
+    throw new Error(`The file build number (${buildNumber}) and the new build number (${news.buildNumber}) do not match!`)
+  }
+
+  const json = `{\n  "version": {\n    "branch": "${BRANCH}",\n    "public": "${news.semanticVersion}",\n    "build": ${buildNumber}\n  }\n}`
+  const filePath = path.join(paths.sourceData.root, "metadata.json");
+  fs.writeFileSync(filePath, json);
+  console.log("Wrote metadata.json to", filePath);
+}
